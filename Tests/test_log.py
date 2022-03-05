37ad4a84eda599b624696cb70f3b8e9d44f8e110
mwatchorn26 <mwatchorn26@gmail.com>
2022-03-04 21:47:52 -0500

    Cleaning code
    Refactoring code (breaking down into smaller functions). Created a bunch of test code templates for the functions in log.py

---
+++ Tests/test_log.py
@@ -0,0 +1,56 @@
+import pywinauto
+from log import *
+
+def test_initialize():
+    pass
+
+def test_isSystemLocked():
+    pass
+
+def test_open_or_create():
+    pass
+
+def test_setFolderPath():
+    pass
+
+def test_writeTodaysSummaryToFile():
+    pass
+
+def test_calculateTodaysSummary():
+    pass
+
+def test_summarizeToday():
+    pass
+
+def test_readInPreviousInfo():
+    pass
+
+def test_appendToLog():
+    pass
+
+def test_getActiveWindowTitle():
+    pass
+
+def test_():
+    pass
+
+def test_():
+    pass
+
+def test_():
+    pass
+
+def test_():
+    pass
+
+def test_():
+    pass
+
+def test_():
+    pass
+
+def test_():
+    pass
+
+def test_():
+    pass
--- log.py
+++ log.py
@@ -14,66 +14,81 @@ import win32evtlog
 import win32security
 import win32evtlogutil
  
-ph = win32api.GetCurrentProcess()
-th = win32security.OpenProcessToken(ph, win32con.TOKEN_READ)
-my_sid = win32security.GetTokenInformation(th, win32security.TokenUser)[0]
- 
-applicationName = "Log Hours"
-eventID = 1
-category = 5	# Shell
-myType = win32evtlog.EVENTLOG_WARNING_TYPE
-descr = ["A warning", "An even more dire warning"]
-data = "Application\0Data".encode("ascii")
- 
-win32evtlogutil.ReportEvent(applicationName, eventID, eventCategory=category, 
-	eventType=myType, strings=descr, data=data, sid=my_sid)
-
+def initialize():
+	print('\n\n\nInitializing...')
+	ph = win32api.GetCurrentProcess()
+	th = win32security.OpenProcessToken(ph, win32con.TOKEN_READ)
+	my_sid = win32security.GetTokenInformation(th, win32security.TokenUser)[0]
 	
-#SETUP ALL OUR BEGINNING STATE VARIABLES
-user = os.getlogin()
-appPath = os.path.normpath("c:/Users/" + user + "/LogHours/")
-scanTime = 60 #SECONDS BETWEEN UPDATES
-previousTitle=""
-last15scans={}
-files={'Temp':'','Hours':'','15minSummary':'','Summary':''}
+	applicationName = "Log Hours"
+	eventID = 1
+	category = 5	# Shell
+	myType = win32evtlog.EVENTLOG_WARNING_TYPE
+	descr = ["A warning", "An even more dire warning"]
+	data = "Application\0Data".encode("ascii")
+	
+	win32evtlogutil.ReportEvent(applicationName, eventID, eventCategory=category, 
+		eventType=myType, strings=descr, data=data, sid=my_sid)
 
+		
+	#SETUP ALL OUR BEGINNING STATE VARIABLES
+	user = os.getlogin()
+	appPath = os.path.normpath("c:/Users/" + user + "/LogHours/")
+	scanTime = 60 #SECONDS BETWEEN UPDATES
+	previousTitle=""
+	last15scans={}
+	files={'Temp':'','Hours':'','15minSummary':'','Summary':''}
+
+	setFolderPath(files)
 
 def summarizeToday():
-    #print('Summarizing Today')
-    #READ IN TODAY'S TASKS INFO
-    fileHandle = open(files['15minSummary'],"r")
-    lineList = fileHandle.readlines()
-    fileHandle.close()
-	
-    taskList={}
-    #CALCULATE TODAY'S SUMMARY
-    for line in lineList:
-        lineParts = line.split("Window:")
-        title = lineParts[1]
-        #UPDATE THE DICTIONARY WITH THIS TASK's TITLE 
-        if title in taskList:
-            taskList[title]=taskList[title]+1
-            #print('Incremented summary time spent on %s' % title)
-        else:
-            taskList[title]=1
-            #print('Added new task: %s' % title)
-    newvar = "astercraft"
-    #WRITE TODAY'S SUMMARY
-    with open(files['Summary'],"w") as summary:
-        try:
-            for task in taskList:
-                summary.write(str(0.25 * taskList[task]) + ' hrs on ' + str(task))
-            summary.write("\n" + str(sum(taskList.values()) * 0.25 ) + " Hours Total")
-            summary.close()
-        except:
-            print('Error occured!')
+	'''
+	'''
+	#READ IN TODAY'S TASKS INFO
+	fileHandle = open(files['15minSummary'],"r")
+	lineList = fileHandle.readlines()
+	fileHandle.close()
+	taskList={}
+	calculateTodaysSummary(lineList, taskList)
+	writeTodaysSummaryToFile(files, taskList)
+
+def calculateTodaysSummary(lineList, taskList):
+	'''
+	Create a list of tasks for today
+	'''
+	#CALCULATE TODAY'S SUMMARY
+	for line in lineList:
+		lineParts = line.split("Window:")
+		title = lineParts[1]
+		#UPDATE THE DICTIONARY WITH THIS TASK's TITLE 
+		if title in taskList:
+			taskList[title]=taskList[title]+1
+			#print('Incremented summary time spent on %s' % title)
+		else:
+			taskList[title]=1
+			#print('Added new task: %s' % title)
+
+def writeTodaysSummaryToFile(files, taskList):
+	'''
+	Overrite the summary file for today.
+	'''
+	#WRITE TODAY'S SUMMARY
+	with open(files['Summary'],"w") as summary:
+		try:
+			for task in taskList:
+				summary.write(str(0.25 * taskList[task]) + ' hrs on ' + str(task))
+			summary.write("\n" + str(sum(taskList.values()) * 0.25 ) + " Hours Total")
+			summary.close()
+		except:
+			print('Error occured!')
 
 def setFolderPath(files):
 	#print('calling setFolderPath')
 	day = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-		
-	if not os.path.exists(appPath+'/'+day):
-		os.makedirs(appPath+'/'+day)
+
+	appPathDay = os.path.join(os.sep, appPath, day)	
+	if not os.path.exists(appPathDay):
+		os.makedirs(appPathDay)
 	
 	files['Latest'] = os.path.join(os.sep, appPath, 'latest.log')
 	files['allTaskChanges'] = os.path.join(os.sep, appPath, day, 'allTaskChanges.log')
@@ -107,76 +122,34 @@ def isSystemLocked():
 	#	print time.asctime (), "still locked"
 	#	time.sleep (2)
 	return not result
-	
-	
-print('\n\n\nInitializing...')
-setFolderPath(files)
-#print(files['Latest'])
-#print(files['allTaskChanges'])
-#print(files['15minSummary'])
-#print(files['Summary'])
 
-#pdb.set_trace()
-
-# try:
-# 	latest = open(r"c:\Users\m_wat\LogHours\latest",'w')
-# 	latest.write("some text")
-# except Exception as e:
-# 	print(e)
-# finally:
-# 	latest.close()
-
-try:
-	fileHandle = open(files['Latest'], 'r')
-except Exception as e:
-	print(e)
-# except IOError:
-# 	with open(files['Latest'],"w") as fileHandle:
-# 		fileHandle.write("Error Reading File")
-fileHandle.close()
-
-while True:
-	
+def readInPreviousInfo():
 	#READ IN THE PREVIOUS INFO
 	fileHandle = open(files['Latest'],"r")
 	lineList = fileHandle.readlines()
 	fileHandle.close()
 	#print(lineList)
 	#print(files['Latest'])
-	
-	#GET THE LAST RECORDED TIME
-	lastTime=str(lineList[-1]).split(' ')[0]
-	#lastTime=str(lineList).split(' ')[0]
-	#print("Last Time: %s" % lastTime)
-	
-	#GET THE CURRENT TIME
-	currentTime=str(time.time()).split('.')[0]
-	#print("Current Time: %r" % currentTime)
+	return lineList
 
+def appendToLog(textToLog, title, previousTitle):
+	with open(files['allTaskChanges'],"a") as log:
+		log.write(textToLog)
+		log.close()
+	previousTitle = title
 
-	#sys.stdout.write(float(currentTime), ' ', float(lastTime))
-	#sys.stdout.flush()
-	#input ("\nPress any key to continue...")
-
-    
-	
-	#CALCULATE THE DIFFERENCE IN TIMES
-	difference = float(currentTime) - float(lastTime)
-
-	#print(difference)
-	
-	#input("\n Waiting...")
-    
+def getActiveWindowTitle():
 	#Get the Window Title
 	w=win32gui
 
 	try:
 		title = w.GetWindowText (w.GetForegroundWindow())
-		testTitle= str(title)
+		print(title)
 	except:
 		print('EXCEPTION:')
 		print("title: %r" % title)
 		print("Unexpected error:", sys.exc_info()[0])
+		title=""
 	#Throw out any web-page specific info, all we want is meta data.
 	if 'Chrome'.lower() in title.lower(): 
 		title="Chrome"
@@ -188,67 +161,99 @@ while True:
 		title="Internet Explorer"
 		#print("found IE")
 	#print("Window Text: " + repr(title)+'\n')
-	
-	#pdb.set_trace()
-	readableTime = time.strftime('(%Y-%m-%d %H:%M:%S)', time.localtime(float(currentTime)))
-		
-	#CHECK FOR A TIME DISCREPENCY
-	if (difference)>(scanTime*2):	
-		#print("\n\nRestarted\n")
-		setFolderPath(files)
-		with open(files['allTaskChanges'],"a") as log:
-			log.write('Started at: ' + str(currentTime)+ '  ' + readableTime + "\n")
-			#log.write(str(currentTime)+ '  ' + readableTime + ' Window:' + title + "\n")
-			log.close()
-		previousTitle = title
-	
-	#CHECK FOR A CHANGE IN TASKS
-	if (previousTitle != title):
-		#print("Task Changed")	
-		with open(files['allTaskChanges'],"a") as log:
-			log.write(str(currentTime)+ '  ' + readableTime + ' Window:' + title + "\n")
-			log.close()
-		previousTitle = title
-		if isSystemLocked():
-			#print('Detected System Locked')
-			time.sleep(scanTime)
-			continue
+	return title
 
-	#UPDATE THE DICTIONARY WITH THE LATEST MINUTE'S TITLE
-	if title in last15scans:
-		last15scans[title]=last15scans[title]+1
-		#print('Incremented time spent on %s' % title)
-	else:
-		last15scans[title]=1
-		#print('Added new task: %s' % title)
+def main():
+	initialize()
+
+	try:
+		fileHandle = open(files['Latest'], 'r')
+	except Exception as e:
+		print(e)
+	# except IOError:
+	# 	with open(files['Latest'],"w") as fileHandle:
+	# 		fileHandle.write("Error Reading File")
+	fileHandle.close()
+
+	while True:
 		
-	#IF IT'S BEEN MORE THAN 15 MINUTES...
-	if (sum(last15scans.values()) >= 15):
-		mainTask = max(last15scans, key=lambda key: last15scans[key])
-		#print("\nPast 15 minutes, main task:%r\n" % mainTask)
-		last15scans={}		
-		with open(files['15minSummary'],"a") as log:
-			log.write(str(currentTime)+ '  ' + readableTime + ' Window:' + mainTask + "\n")
-			log.close()
+		lineList = readInPreviousInfo()
+	
+		lastTime, currentTime, difference = get
+
+		#GET THE LAST RECORDED TIME
+		lastTime=str(lineList[-1]).split(' ')[0]
+		#lastTime=str(lineList).split(' ')[0]
+		#print("Last Time: %s" % lastTime)
 		
-		sys.stdout.write("-")
+		#GET THE CURRENT TIME
+		currentTime=str(time.time()).split('.')[0]
+		#print("Current Time: %r" % currentTime)
+
+		#CALCULATE THE DIFFERENCE IN TIMES
+		difference = float(currentTime) - float(lastTime)
+
+		#print(difference)
+		
+		#input("\n Waiting...")
+		
+		title = getActiveWindowTitle()
+		
+		#pdb.set_trace()
+		readableTime = time.strftime('(%Y-%m-%d %H:%M:%S)', time.localtime(float(currentTime)))
+			
+		#CHECK FOR A TIME DISCREPENCY (Like when the program has crashed for a long time, and is restarted)
+		if (difference)>(scanTime*2):
+			setFolderPath(files)
+			appendToLog('Started at: ' + str(currentTime)+ '  ' + readableTime + "\n", title, previousTitle)
+			#previousTitle = title
+		
+		#CHECK FOR A CHANGE IN TASKS
+		if (previousTitle != title):
+			appendToLog(str(currentTime) + '  ' + readableTime + ' Window:' + title + "\n")
+			#previousTitle = title
+			if isSystemLocked():
+				time.sleep(scanTime)
+				continue
+
+		#UPDATE THE DICTIONARY WITH THE LATEST MINUTE'S TITLE
+		if title in last15scans:
+			last15scans[title]=last15scans[title]+1
+			#print('Incremented time spent on %s' % title)
+		else:
+			last15scans[title]=1
+			#print('Added new task: %s' % title)
+			
+		#IF IT'S BEEN MORE THAN 15 MINUTES...
+		if (sum(last15scans.values()) >= 15):
+			mainTask = max(last15scans, key=lambda key: last15scans[key])
+			#print("\nPast 15 minutes, main task:%r\n" % mainTask)
+			last15scans={}		
+			with open(files['15minSummary'],"a") as log:
+				log.write(str(currentTime)+ '  ' + readableTime + ' Window:' + mainTask + "\n")
+				log.close()
+			
+			sys.stdout.write("-")
+			sys.stdout.flush()
+			
+			
+		#WRITE THE LATEST VALUES TO TEMPORARY FILE
+		with open(files['Latest'],"w") as temp:
+			try:
+				temp.write(str(currentTime)+ '  ' + readableTime + ' Window:' + title)
+			except:
+				print('EXCEPTION:')
+				print("title: %r" % title)
+				print("Unexpected error:", sys.exc_info()[0])
+			temp.close()
+			
+		#SUMMARIZE TODAY, JUST IN CASE WE LOSE POWER NOW
+		summarizeToday()
+		
+		sys.stdout.write(".")
 		sys.stdout.flush()
 		
-		
-	#WRITE THE LATEST VALUES TO TEMPORARY FILE
-	with open(files['Latest'],"w") as temp:
-		try:
-			temp.write(str(currentTime)+ '  ' + readableTime + ' Window:' + title)
-		except:
-			print('EXCEPTION:')
-			print("title: %r" % title)
-			print("Unexpected error:", sys.exc_info()[0])
-		temp.close()
-		
-	#SUMMARIZE TODAY, JUST IN CASE WE'RE SHUTDOWN THIS INSTANT
-	summarizeToday()
-	
-	sys.stdout.write(".")
-	sys.stdout.flush()
-	
-	time.sleep(scanTime)
+		time.sleep(scanTime)
+
+if __name__ == "__main__":
+    main()
